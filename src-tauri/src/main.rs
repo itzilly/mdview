199661f1dde5641428b235e74f05035205fb33ca
illyum <90023277+itzilly@users.noreply.github.com>
2024-03-05 06:20:27 -0700

    Allow user to open application without file, then display 'Open File'

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,41 @@
 // Removes console
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
+use std::fs;
 use std::env;
+use std::process;
 use tauri::AppHandle;
 
 
 #[tauri::command]
 async fn get_filename(app: AppHandle) -> Result<String, String> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        return Err("Usage: mdview <FilePath.md>".to_string());
+        if args.len() < 2 {
+        return Ok::<String, String>(String::new());
     }
     Ok(args[1].clone())
 }
 
+#[tauri::command]
+async fn file_exists(path: &str) -> Result<bool, String> {
+    let result = match std::fs::metadata(&path) {
+        Ok(_) => {
+            Ok(true)
+        },
+        Err(_) => {
+            Ok(false)
+        },
+    };
+    result
+}
+
 fn main() {
+    launch_app();
+}
+
+fn launch_app() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_filename])
-        .run(tauri::generate_context!())
-        .expect("Error while running tauri application... Who knows why");
+    .invoke_handler(tauri::generate_handler![get_filename, file_exists])
+    .run(tauri::generate_context!())
+    .expect("Error while running tauri application... Who knows why");
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,72 @@
 const { invoke } = window.__TAURI__.tauri;
 const { readTextFile } = window.__TAURI__.fs;
+const { open } = window.__TAURI__.dialog;
 
 
 let filePath;
+let fileExists;
+
 
 async function set_filename() {
   filePath = await invoke("get_filename");
+}
+
+async function check_file_exists() {
+  fileExists = await invoke("file_exists", { path: filePath });
 }
 
 async function get_file_contents(path) {
   return await readTextFile(filePath);
 }
 
-async function display_file() {
-  await set_filename();
-  let contents = await get_file_contents(filePath);
-  let html = marked.parse(contents);
-  let contentElement = document.getElementById("markdown-div");
-  contentElement.innerHTML = DOMPurify.sanitize(html);
+async function show_default_file() {
+  const defaultHTML = `
+  <div class="centered">
+      <p>Welcome to MDView! Press the button below to open a file</p>
+      <button id="choose-file-button">Choose File</button>
+      <p>Source on <a href="https://github.com/itzilly/mdview">Github</a>  |🖇|  Created by <a href="www.itzilly.com">itzilly</a>  |🖇|  © 2024 illyum</p>
+  </div>
+`;
+  document.getElementById("markdown-div").innerHTML = defaultHTML;
 
+  document.getElementById("choose-file-button").addEventListener("click", async () => {
+    const result = await open({
+        multiple: false,
+        directory: false,
+        filter: 'All Files',
+    });
+
+    // Check if a file was selected
+    if (!result.cancelled) {
+      filePath = result;
+      await check_file_exists();
+      await display_file();
+    }
+});
+}
+
+async function display_file() {
+  if (filePath !== undefined) {
+    await check_file_exists()
+    if (fileExists) {
+      let contents = await get_file_contents(filePath);
+      let html = marked.parse(contents);
+      let contentElement = document.getElementById("markdown-div");
+      contentElement.innerHTML = DOMPurify.sanitize(html);
+      contentElement.querySelector('.centered').classList.remove('centered')
+    } else {
+      console.log("Showing default file - failed exists check")
+      await show_default_file();
+    }
+  } else {
+    console.log("Showing default file - fauled undefined check")
+    await show_default_file();
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   (async () => {
+    await set_filename();
     await display_file();
   })();
 })
